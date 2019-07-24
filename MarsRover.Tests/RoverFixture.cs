b1b161850909f98d1a97298ce4477b494b453fdb
Sundesh Mahesh Chawla <51902386+tavisca-schawla@users.noreply.github.com>
2019-07-25 00:18:23 +0530

    Added basic rover implementation

--- MarsRover.Tests/RoverFixture.cs
+++ MarsRover.Tests/RoverFixture.cs
@@ -1,3 +1,4 @@
+using FluentAssertions;
 using System;
 using Xunit;
 
@@ -6,9 +7,24 @@ namespace MarsRover.Tests
     public class RoverFixture
     {
         [Fact]
-        public void Rover_should_have__valid_co_ordinates()
+        public void Rover_should_have_valid_intial_co_ordinates()
         {
-            
+            Rover rover = new Rover(2,3, Direction.E);
+            rover.Position.Should().BeEquivalentTo(new Position { X = 2, Y = 3 });
+        }
+
+        [Fact]
+        public void Rover_should_be_facing_a_valid_direction()
+        {
+            Rover rover = new Rover(2, 3, Direction.E);
+            rover.Compass.Direction.Should().BeEquivalentTo(Direction.E);
+        }
+
+        [Fact]
+        public void Rover_should_be_able_to_receive_instructions()
+        {
+            Rover rover = new Rover(2, 3, Direction.E);
+
         }
     }
 }
--- MarsRover/Rover.cs
+++ MarsRover/Rover.cs
@@ -5,5 +5,13 @@ namespace MarsRover
     public class Rover
     {
         public Position Position { get; set; }
+
+        public Compass Compass { get; set; }
+
+        public Rover(int x, int y, Direction direction)
+        {
+            this.Position = new Position { X = x, Y = y };
+            this.Compass = new Compass(direction);
+        }
     }
 }
