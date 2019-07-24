1b01387cf9cfe689e687fb877d71442f01a6706d
Sundesh Mahesh Chawla <51902386+tavisca-schawla@users.noreply.github.com>
2019-07-25 00:19:46 +0530

    Added Utility and Position classes

---
+++ MarsRover/Position.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MarsRover
+{
+    public class Position
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+}
---
+++ MarsRover/Utility.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MarsRover
+{
+    public enum Direction { W, N, E, S };
+
+    public enum Instruction { L = -1, M, R };
+}
