70d056c49e543d2fb121b311f9e20ce5ac9a7ee5
Sundesh Mahesh Chawla <51902386+tavisca-schawla@users.noreply.github.com>
2019-07-24 23:43:51 +0530

    Added basic compass functionality

---
+++ MarsRover.Tests/CompassFixture.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace MarsRover.Tests
+{
+    public class CompassFixture
+    {
+        [Fact]
+        public void Compass_should_be_able_to_set_initial_value()
+        {
+            var compass = new Compass(Direction.E);
+            compass.Direction.Should().Be(Direction.E);
+        }
+
+        //[Fact]
+        //public void Compass_should_be_able_to_change_direction()
+        //{
+        //    var compass = new Compass(Direction.E);            
+        //    compass.Direction.Should().Be(Direction.E);
+        //}
+    }
+}
---
+++ MarsRover/Compass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MarsRover
+{
+    public class Compass
+    {
+        public Direction Direction { get; set; }
+
+        public Compass(Direction direction)
+        {
+            this.Direction = direction;
+        }
+    }
+}
