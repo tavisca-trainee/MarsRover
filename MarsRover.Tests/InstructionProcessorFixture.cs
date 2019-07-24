56f32024aa04739fc66aded0fb2b5df4999a407e
Sundesh Mahesh Chawla <51902386+tavisca-schawla@users.noreply.github.com>
2019-07-25 00:19:03 +0530

    Added basic instruction processor implementation.

---
+++ MarsRover.Tests/InstructionProcessorFixture.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace MarsRover.Tests
+{
+    public class InstructionProcessorFixture
+    {
+        [Fact]
+        public void Instruction_processor_should_be_able_to_receive_instructions()
+        {
+            InstructionProcessor instructionProcessor = new InstructionProcessor();
+            instructionProcessor.GetInstructions("LLMRLRMRLRMRRMM");
+            instructionProcessor.Instructions.Should().BeEquivalentTo(new List<Instruction>
+            {
+                Instruction.L,
+                Instruction.L,
+                Instruction.M,
+                Instruction.R,
+                Instruction.L,
+                Instruction.R,
+                Instruction.M,
+                Instruction.R,
+                Instruction.L,
+                Instruction.R,
+                Instruction.M,
+                Instruction.R,
+                Instruction.R,
+                Instruction.M,
+                Instruction.M
+            });
+        }
+
+
+    }
+}
---
+++ MarsRover/InstructionProcessor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MarsRover
+{
+    public class InstructionProcessor
+    {
+        public List<Instruction> Instructions { get; set; }
+
+        public InstructionProcessor()
+        {
+            this.Instructions = new List<Instruction>();
+        }
+
+        public void GetInstructions(string instructions)
+        {
+            for (int i = 0; i < instructions.Length; i++)
+            {
+                Instructions.Add((Instruction)Enum.Parse(typeof(Instruction), instructions[i].ToString()));
+            }
+        }
+    }
+}
